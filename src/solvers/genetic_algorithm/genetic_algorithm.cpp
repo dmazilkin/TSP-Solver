7de319201d54cd62528016385027aac0c2c230f2
Dmitrii Mazilkin <dim.mazilkin@gmail.com>
2025-05-28 22:05:41 +0200

    Add fitness sorting.

--- src/solvers/genetic_algorithm/genetic_algorithm.cpp
+++ src/solvers/genetic_algorithm/genetic_algorithm.cpp
@@ -13,7 +13,6 @@ void GeneticAlgorithm::solve(std::vector<std::vector<float>> dist)
 {
     std::vector<std::vector<int>> population = initialize_population_(POPULATION_SIZE, dist.size());
     std::vector<float> population_fitness = calc_fitness_(population, dist);
-
 }
 
 /******************** PRIVATE ********************/
@@ -22,6 +21,7 @@ std::vector<std::vector<int>> GeneticAlgorithm::initialize_population_ (int popu
     std::vector<std::vector<int>> population(population_size, std::vector<int>(individual_size, 0));
 
     for (int i = 0; i < population_size; i++) {
+
         for (int j = 0; j < individual_size; j++) {
             population[i][j] = j;
         }
@@ -39,17 +39,22 @@ std::vector<float> GeneticAlgorithm::calc_fitness_ (std::vector<std::vector<int>
     std::vector<float> fitness (POPULATION_SIZE, 0);
 
     for (int i = 0; i < POPULATION_SIZE; i++) {
+
         for (int j = 0; j < population[0].size(); j++) {
+
             if (j == POPULATION_SIZE - 1) {
                 int current = population[i][j];
                 int next = population[i][0];
                 fitness[i] += dist[current][next];
             }
+
             int current = population[i][j];
             int next = population[i][j + 1];
             fitness[i] += dist[current][next];
         }
     }
+
+    std::sort(fitness.begin(), fitness.end());
 
     return fitness;
 }
